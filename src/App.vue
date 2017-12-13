4ea3138d3e3bd56a5b56c58673e55e2d5c1be9dd
Jiaren Liu <jrliu@alauda.io>
2017-12-13 11:45:49 +0800

    bump dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,84 @@
 <template lang="pug">
-  #app.container(:class="$style.container")
-    h4 Demo:
-    button.btn.btn-default(@click="basicModal") Basic Modal
-    button.btn.btn-default(@click="basicModal5") Basic Modal 5 times
-    button.btn.btn-default(@click="alert") Alert Modal
-    button.btn.btn-default(@click="confirm") Confirm Modal
-    button.btn.btn-default(@click="prompt") Prompt Modal
-    button.btn.btn-default(@click="toast") Toast Modal
-    modal#modal
+#app.container(:class="$style.container")
+  h4 Demo:
+  button.btn.btn-default(@click="basicModal") Basic Modal
+  button.btn.btn-default(@click="basicModal5") Basic Modal 5 times
+  button.btn.btn-default(@click="alert") Alert Modal
+  button.btn.btn-default(@click="confirm") Confirm Modal
+  button.btn.btn-default(@click="prompt") Prompt Modal
+  button.btn.btn-default(@click="toast") Toast Modal
+  modal#modal
 </template>
-<script lang="js">
-  import Modal from 'Modal'
+<script>
+import Modal from 'Modal'
 
-  import tip from 'tip'
+import tip from 'tip'
 
-  const {alert, confirm, prompt, toast} = tip
+const { alert, confirm, prompt, toast } = tip
 
-  export default {
-    methods: {
-      basicModal(options) {
-        this.$modal.open({
+export default {
+  methods: {
+    basicModal(options) {
+      this.$modal
+        .open({
           component: import('BasicModal'),
-          options
-        }).then(modal => {
+          options,
+        })
+        .then(modal => {
           console.log(modal.id)
         })
-      },
-      basicModal5() {
-        this.basicModal()
-        const interval = setInterval(() => this.basicModal({backdrop: 'static'}), 2e3)
-        setTimeout(() => clearInterval(interval), 9e3)
-      },
-      alert() {
-        alert(`I'm Alert`)
-      },
-      confirm() {
-        confirm({
-          tipText: `I'm Confirm`,
-          confirm() {
-            this.$modal.close().then(() => alert('Alert after confirm'))
-          }
-        })
-      },
-      prompt() {
-        prompt({
-          tipText: '还不赶紧写原因？',
-          placeholder: '在此填写原因 (50字以内)，或者直接点击"确定"',
-          confirm: text => alert(text)
-        })
-      },
-      toast() {
-        toast(`I'm Toast`)
-      }
     },
-    components: {
-      Modal
-    }
-  }
+    basicModal5() {
+      this.basicModal()
+      const interval = setInterval(
+        () => this.basicModal({ backdrop: 'static' }),
+        2e3,
+      )
+      setTimeout(() => clearInterval(interval), 9e3)
+    },
+    alert() {
+      alert(`I'm Alert`)
+    },
+    confirm() {
+      confirm({
+        tipText: `I'm Confirm`,
+        confirm() {
+          this.$modal.close().then(() => alert('Alert after confirm'))
+        },
+      })
+    },
+    prompt() {
+      prompt({
+        tipText: '还不赶紧写原因？',
+        placeholder: '在此填写原因 (50字以内)，或者直接点击"确定"',
+        confirm: text => alert(text),
+      })
+    },
+    toast() {
+      toast(`I'm Toast`)
+    },
+  },
+  components: {
+    Modal,
+  },
+}
 </script>
 <style src="bootstrap" lang="stylus"></style>
 <style src="transition" lang="stylus"></style>
 <style lang="stylus" module>
-  .container
-    :global
-      .theme-color
-        color #b28ef2 !important
+.container {
+  :global {
+    .theme-color {
+      color: #b28ef2 !important;
+    }
 
-      .btn
-        margin-bottom 10px
+    .btn {
+      margin-bottom: 10px;
 
-        &:not(:last-child)
-          margin-right 10px
+      &:not(:last-child) {
+        margin-right: 10px;
+      }
+    }
+  }
+}
 </style>
--- src/BasicModal.vue
+++ src/BasicModal.vue
@@ -1,43 +1,46 @@
 <template lang="pug">
-  modal-item(:class="$style.container", header="Basic Modal")
-    .text-center(:class="$style.clear")
-      button.btn.btn-primary(@click="$modal.closeAll") Clear Modals
-    .row
-      .col-md-4 .col-md-4
-      .col-md-4.col-md-offset-4 .col-md-4 .col-md-offset-4
-    .row
-      .col-md-3.col-md-offset-3 .col-md-3 .col-md-offset-3
-      .col-md-2.col-md-offset-4 .col-md-2 .col-md-offset-4
-    .row
-      .col-md-6.col-md-offset-3 .col-md-6 .col-md-offset-3
-    .row
-      .col-sm-9
-        | Level 1: .col-sm-9
-        .row
-          .col-xs-8.col-sm-6
-            | Level 2: .col-xs-8 .col-sm-6
-          .col-xs-4.col-sm-6
-            | Level 2: .col-xs-4 .col-sm-6
+modal-item(:class="$style.container", header="Basic Modal")
+  .text-center(:class="$style.clear")
+    button.btn.btn-primary(@click="$modal.closeAll") Clear Modals
+  .row
+    .col-md-4 .col-md-4
+    .col-md-4.col-md-offset-4 .col-md-4 .col-md-offset-4
+  .row
+    .col-md-3.col-md-offset-3 .col-md-3 .col-md-offset-3
+    .col-md-2.col-md-offset-4 .col-md-2 .col-md-offset-4
+  .row
+    .col-md-6.col-md-offset-3 .col-md-6 .col-md-offset-3
+  .row
+    .col-sm-9
+      | Level 1: .col-sm-9
+      .row
+        .col-xs-8.col-sm-6
+          | Level 2: .col-xs-8 .col-sm-6
+        .col-xs-4.col-sm-6
+          | Level 2: .col-xs-4 .col-sm-6
 </template>
 <script>
-  import ModalItem from 'ModalItem'
+import ModalItem from 'ModalItem'
 
-  export default {
-    components: {
-      ModalItem
-    }
-  }
+export default {
+  components: {
+    ModalItem,
+  },
+}
 </script>
 <style lang="stylus" module>
-  .container
-    :global([class*=col-])
-      padding-top: 10px
-      padding-bottom: 10px
-      background-color: #eee
-      background-color: rgba(86, 61, 124, .15)
-      border: 1px solid #ddd
-      border: 1px solid rgba(86, 61, 124, .2)
+.container {
+  :global([class*=col-]) {
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background-color: #eee;
+    background-color: rgba(86, 61, 124, 0.15);
+    border: 1px solid #ddd;
+    border: 1px solid rgba(86, 61, 124, 0.2);
+  }
+}
 
-  .clear
-    padding-bottom 15px
+.clear {
+  padding-bottom: 15px;
+}
 </style>
